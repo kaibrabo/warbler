ec6c09af74b4fc21f45b5a5636156bfa5e562a81
Kainoa Brabo <kaibrabo@gmail.com>
2019-10-19 01:12:41 -0700

    format documents

--- warbler-server/handlers/messages.js
+++ warbler-server/handlers/messages.js
@@ -2,52 +2,55 @@ const db = require("../models");
 
 // POST - /api/users/:id/messages/
 exports.createMessage = async function(req, res, next) {
-  try {
-    // Creates message
-    let message = await db.Message.create({
-      text: req.body.text,
-      user: req.params.id
-    });
+	try {
+		// Creates message
+		let message = await db.Message.create({
+			text: req.body.text,
+			user: req.params.id
+		});
 
-    // Finds user
-    let foundUser = await db.User.findById(req.params.id);
-    // Add message.id to user's message arr
-    foundUser.messages.push(message.id);
-    await foundUser.save();
+		// Finds user
+		let foundUser = await db.User.findById(req.params.id);
+		// Add message.id to user's message arr
+		foundUser.messages.push(message.id);
+		await foundUser.save();
 
-    let foundMessage = await db.Message.findById(message._id).populate("user", {
-      username: true,
-      profileImageUrl: true
-    });
+		let foundMessage = await db.Message.findById(message._id).populate(
+			"user",
+			{
+				username: true,
+				profileImageUrl: true
+			}
+		);
 
-    let successfulMessage = await res.status(200).json(foundMessage);
+		let successfulMessage = await res.status(200).json(foundMessage);
 
-    return successfulMessage;
-  } catch (err) {
-    return next(err);
-  }
+		return successfulMessage;
+	} catch (err) {
+		return next(err);
+	}
 };
 
 // GET - /api/users/:id/messages/:message_id
 exports.getMessage = async function(req, res, next) {
-  try {
-    let message = await db.Message.find(req.params.message_id);
+	try {
+		let message = await db.Message.find(req.params.message_id);
 
-    return res.status(200).json(message);
-  } catch (err) {
-    return next(err);
-  }
+		return res.status(200).json(message);
+	} catch (err) {
+		return next(err);
+	}
 };
 
 // DELETE - /api/users/:id/messages/:message_id
 exports.deleteMessage = async function(req, res, next) {
-  try {
-    let foundMessage = await db.Message.findById(req.params.message_id);
+	try {
+		let foundMessage = await db.Message.findById(req.params.message_id);
 
-    await foundMessage.remove();
+		await foundMessage.remove();
 
-    return res.status(200).json(foundMessage);
-  } catch (err) {
-    return next(err);
-  }
+		return res.status(200).json(foundMessage);
+	} catch (err) {
+		return next(err);
+	}
 };
